9b8a0d1c6073052f789157ce32fe359fb43433f2
sumichoi <97506292+sumichoi@users.noreply.github.com>
2022-10-07 18:26:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-// 숫자형 변수의 덧셈
-const myArray = ['펭귄', '고래', '참치'];
-myArray[0] = '새우';
 
-console.log(myArray); // 결과：30
+$("#calc").click(function() {
 
-// 문자열 변수의 결합
-const myObject = { id: 20, name: '펭귄' };
-myObject.name = '오리';
-console.log(myObject); // 결과：'브렌던아이크'
 
-console.log('사과' == '사과'); // 값이 같으므로 true 반환
-console.log(10 < 30); // true
-console.log(20 >= 30); // false
+var x = $("#inputTime").val();
+  document.getElementById("score_badge").innerHTML = x;
+
+})
